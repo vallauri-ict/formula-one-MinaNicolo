58fa86a2c95591ca79ee3ca8f8a085df5439cae0
Stud. Pietro Gariuolo <p.gariuolo.0893@vallauri.net>
2020-10-15 13:19:08 +0200

    Add function Reset DB

--- FormulaOneConsole/Program.cs
+++ FormulaOneConsole/Program.cs
@@ -9,7 +9,7 @@ namespace FormulaOneConsole
         public const string WORKINGPATH = @"C:\data\formulaone\";
         private const string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + WORKINGPATH + @"FormulaOne.mdf;Integrated Security=True";
         static void Main(string[] args)
-        {          
+        {
             char scelta = ' ';
             do
             {
@@ -17,6 +17,8 @@ namespace FormulaOneConsole
                 Console.WriteLine("1 - Create Countries");
                 Console.WriteLine("2 - Create Teams");
                 Console.WriteLine("3 - Create Drivers");
+                Console.WriteLine("------------------");
+                Console.WriteLine("R - Reset DB");
                 Console.WriteLine("------------------");
                 Console.WriteLine("X - EXIT\n");
                 scelta = Console.ReadKey(true).KeyChar;
@@ -31,6 +33,26 @@ namespace FormulaOneConsole
                     case '3':
                         callExecuteSqlScript("Drivers");
                         break;
+                    case 'R':
+                        bool OK;
+                        //System.IO.File.Copy(DbTools.WORKINGPATH + "FormulaOne.mdf", DbTools.WORKINGPATH + "Backup.mdf");
+                        OK = callDropTable("Countries");
+                        if (OK) OK = callDropTable("Teams");
+                        if (OK) OK = callDropTable("Drivers");
+                        if (OK) OK = callExecuteSqlScript("Countries");
+                        if (OK) OK = callExecuteSqlScript("Drivers");
+                        if (OK) OK = callExecuteSqlScript("Teams");
+                        if (OK)
+                        {
+                            //System.IO.File.Delete(DbTools.WORKINGPATH + "Backup.mdf");
+                            Console.WriteLine("RESET DB OK");
+                        }
+                        else
+                        {
+                            //System.IO.File.Copy(DbTools.WORKINGPATH + "Backup.mdf", DbTools.WORKINGPATH + "FormulaOne.mdf", true);
+                            //System.IO.File.Delete(DbTools.WORKINGPATH + "Backup.mdf");
+                        }
+                        break;
                     case 'X': break;
 
                     default:
@@ -38,6 +60,36 @@ namespace FormulaOneConsole
                         break;
                 }
             } while (scelta != 'X' && scelta != 'x');
+        }
+
+        static bool callDropTable(string tableName)
+        {
+            try
+            {
+                DropTable(tableName);
+                Console.WriteLine("\nDROP " + tableName + " - SUCCESS\n");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nDROP " + tableName + " - ERROR: " + ex.Message + "\n");
+                return false;
+            }
+        }
+        private static void DropTable(string tableName)
+        {
+            var con = new SqlConnection(CONNECTION_STRING);
+            var cmd = new SqlCommand("Drop Table " + tableName + ";", con);
+            con.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException err)
+            {
+                Console.WriteLine("\tErrore SQL: " + err.Number + " - " + err.Message);
+            }
+            con.Close();
         }
 
         public static bool callExecuteSqlScript(string scriptName)
@@ -53,7 +105,7 @@ namespace FormulaOneConsole
                 Console.WriteLine("\nCreate " + scriptName + " - ERROR: " + ex.Message + "\n");
                 return false;
             }
-        }     
+        }
 
         public static void ExecuteSqlScript(string sqlScriptName)
         {
@@ -80,7 +132,7 @@ namespace FormulaOneConsole
                     Console.WriteLine("\tErrore SQL: " + err.Number + " - " + err.Message);
                 }
             }
-            string finalMessage = nErr == 0 ? "Script completed without error" : "Script completed with "+nErr+" ERRORS";
+            string finalMessage = nErr == 0 ? "Script completed without error" : "Script completed with " + nErr + " ERRORS";
             con.Close();
         }
     }
